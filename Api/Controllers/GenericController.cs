8b60bc78f8c8f200795957bafac645acbcb90493
Kleiton Costa <kleiton.costa@dekra.com>
2019-07-25 14:35:25 -0300

    Added Controller to receive non formated data in JSON and XMLformats in the same route/endpoint

---
+++ Api/Controllers/GenericController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class GenericController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("/api/Generic")]
+        [Consumes("application/json")]
+        public IActionResult ReceiveJson([FromBody]object data)
+        {
+            // Do something with a Json not Serialized
+
+            return Ok(data);
+        }
+
+        [HttpPost]
+        [Route("/api/Generic")]
+        [Consumes("application/xml")]
+        public IActionResult ReceiveXML([FromBody]string data)
+        {
+            // Do something with an XML not Serialized
+            // Import - Uses CDATA to send XML like
+            // <string><![CDATA[<contentToRead></contentToRead>]]></string>
+
+            return Ok(data);
+        }
+    }
+}
